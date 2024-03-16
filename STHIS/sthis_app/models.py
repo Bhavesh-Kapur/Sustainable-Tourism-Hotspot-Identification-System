c116a40b985b370788eb4476ccf0b5ac996669fa
Bhavesh-Kapur <Bhavesh.105635@stu.upes.ac.in>
2024-03-17 02:59:06 +0530

    location model

--- STHIS/sthis_app/models.py
+++ STHIS/sthis_app/models.py
@@ -9,3 +9,11 @@ class User(models.Model):
     country = models.CharField(max_length=100)
 
 
+
+class location(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=1000)
+    add = models.CharField(max_length=200)
+    acti = models.CharField(max_length=1000)
+    culture = models.CharField(max_length=100000)
+
